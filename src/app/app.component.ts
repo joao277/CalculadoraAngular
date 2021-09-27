91a54859bbbaf4c6287efa41edb7e7e8d7a9dada
joão277 <joaobstarocha@gmail.com>
2021-09-27 14:06:48 -0300

    CalculadoraAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Calculator';
+  title = 'Calculadora';
+
+  public num1!: number;
+  public num2!: number;
+  public num3!: number;
+
+  adicao(){
+    this.num3= this.num1+this.num2;
+  }
+
+  subtracao(){
+    this.num3= this.num1-this.num2;
+  }
+
+  multiplicacao(){
+    this.num3= this.num1*this.num2;
+  }
+
+  divisao(){
+    this.num3= this.num1/this.num2;
+  }
+
 }

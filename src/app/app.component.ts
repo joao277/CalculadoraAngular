1625129aa87cfc5b03978c83237b305253bec7c6
joão277 <joaobstarocha@gmail.com>
2021-09-28 21:24:55 -0300

    Commit antes da entrega

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Calculadora';
 
-  public num1!: number;
-  public num2!: number;
-  public num3!: number;
+  public num1: number = 0;
+  public num2: number = 0;
+  public num3: number = 0;
 
   adicao(){
-    this.num3= this.num1+this.num2;
+    this.num3 = this.num1+this.num2;
   }
 
   subtracao(){
-    this.num3= this.num1-this.num2;
+    this.num3 = this.num1-this.num2;
   }
 
   multiplicacao(){
-    this.num3= this.num1*this.num2;
+    this.num3 = this.num1*this.num2;
   }
 
   divisao(){
-    this.num3= this.num1/this.num2;
+    this.num3 = this.num1/this.num2;
+  }
+
+  limpar(){
+    this.num1 = 0;
+    this.num2 = 0;
+    this.num3 = 0
   }
 
 }
